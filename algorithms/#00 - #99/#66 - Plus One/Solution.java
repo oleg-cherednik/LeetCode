fb0a9934dde80c0e6abbc598aca1e2d9a48306ea
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-04-03 22:49:28 +0300

    176. Second Highest Salary

--- algorithms/#00 - #99/#66 - Plus One/Solution.java
+++ algorithms/#00 - #99/#66 - Plus One/Solution.java
@@ -27,9 +27,9 @@ import java.util.Arrays;
 public class Solution {
 
     public static void main(String... args) {
-//        System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 3 })));    // [1, 2, 4]
-//        System.out.println(Arrays.toString(plusOne(new int[] { 4, 3, 2, 1 }))); // [4, 3, 2, 2]
-//        System.out.println(Arrays.toString(plusOne(new int[] { 9 })));          // [1, 0]
+        System.out.println(Arrays.toString(plusOne(new int[] { 1, 2, 3 })));    // [1, 2, 4]
+        System.out.println(Arrays.toString(plusOne(new int[] { 4, 3, 2, 1 }))); // [4, 3, 2, 2]
+        System.out.println(Arrays.toString(plusOne(new int[] { 9 })));          // [1, 0]
         System.out.println(Arrays.toString(
                 plusOne(new int[] { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0,
                         6 })));          // [7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 7]
