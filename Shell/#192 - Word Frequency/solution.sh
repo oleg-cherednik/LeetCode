ae2049c87e7444de1d92dd8e5c88dfde227dbf65
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-04-20 22:47:56 +0300

    192. Word Frequency

---
+++ Shell/#192 - Word Frequency/solution.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+declare -A map
+
+while IFS= read -r line
+do
+  for word in $line
+  do
+    let map[$word]=${map[$word]}+1
+  done
+done < words.txt
+
+for word in ${!map[@]}
+do
+  echo $word ${map[$word]}
+done | sort -rn -k2
