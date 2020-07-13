c8842d166b3c1f4af9542c5504d988f61b21d169
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-07-13 09:33:40 +0300

    190. Reverse Bits

--- Algorithms/#100 - #199/#190 - Reverse Bits/Solution.java
+++ Algorithms/#100 - #199/#190 - Reverse Bits/Solution.java
@@ -39,7 +39,7 @@ public class Solution {
 
     public static int reverseBits(int n) {
         n = (n >>> 16) | (n << 16);
-        n = ((n & 0xFF00ff00) >>> 8) | ((n & 0x00FF00FF) << 8);
+        n = ((n & 0xFF00FF00) >>> 8) | ((n & 0x00FF00FF) << 8);
         n = ((n & 0xF0F0F0F0) >>> 4) | ((n & 0x0F0F0F0F) << 4);
         n = ((n & 0xCCCCCCCC) >>> 2) | ((n & 0x33333333) << 2);
         n = ((n & 0xAAAAAAAA) >>> 1) | ((n & 0x55555555) << 1);
