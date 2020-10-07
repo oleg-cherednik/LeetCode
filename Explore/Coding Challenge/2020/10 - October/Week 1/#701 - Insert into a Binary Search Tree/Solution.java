efb1a058c4e90ec769baf730ef0b1c700819eec7
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-10-08 01:01:58 +0300

    701. Insert into a Binary Search Tree

--- Explore/Coding Challenge/2020/10 - October/Week 1/#701 - Insert into a Binary Search Tree/Solution.java
+++ Explore/Coding Challenge/2020/10 - October/Week 1/#701 - Insert into a Binary Search Tree/Solution.java
@@ -24,7 +24,7 @@ import java.util.Queue;
  * <pre>
  * Input: root = [40,20,60,10,30,50,70], val = 25
  * Output: [40,20,60,10,30,50,70,null,null,25]
- * </?pre>
+ * </pre>
  * <b>Example 3:</b>
  * <pre>
  * Input: root = [4,2,7,1,3,null,null,null,null,null,null], val = 5
