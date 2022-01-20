0019605c318628dc748c13b68712a87f50af8c41
Oleg Cherednik <oleg.cherednik@gmail.com>
2022-01-20 11:08:53 +0300

    204. Count Primes

--- Algorithms/#0200 - #0299/#204 - Count Primes/Solution.java
+++ Algorithms/#0200 - #0299/#204 - Count Primes/Solution.java
@@ -14,30 +14,36 @@
 public class Solution {
 
     public static void main(String... args) {
-        System.out.println(countPrimes(10));    // 4
-        System.out.println(countPrimes(2));     // 0
+        System.out.println(countPrimes(0));         // 0
+        System.out.println(countPrimes(1));         // 0
+        System.out.println(countPrimes(2));         // 0
+        System.out.println(countPrimes(3));         // 1
+        System.out.println(countPrimes(10));        // 4
+        System.out.println(countPrimes(1_500_000)); // 114155
+        System.out.println(countPrimes(5_000_000)); // 348514
     }
 
     public static int countPrimes(int n) {
-        int res = 0;
+        if (n < 3)
+            return 0;
 
-        for (int i = 2; i < n; i++)
-            if (isPrime(i))
-                res++;
+        boolean[] notPrimes = new boolean[n];
+        int res = n / 2;
+
+        for (int i = 3; i * i < n; i += 2) {
+            if (notPrimes[i])
+                continue;
+
+            for (int j = i * i; j < n; j += 2 * i) {
+                if (notPrimes[j])
+                    continue;
+
+                notPrimes[j] = true;
+                res--;
+            }
+        }
 
         return res;
     }
 
-    private static boolean isPrime(int n) {
-        if (n == 0)
-            return false;
-        if (n == 1)
-            return true;
-
-        for (int i = 2, sqrt = (int)Math.sqrt(n); i <= sqrt; i++)
-            if (n % i == 0)
-                return false;
-
-        return true;
-    }
 }
