27962fdf61309cef32d986584d9c1f02b56fabed
Oleg Cherednik <oleg.cherednik@gmail.com>
2025-08-16 10:41:21 +0300

    32. Longest Valid Parentheses

--- Algorithms/#0000 - #0099/#32 - Longest Valid Parentheses/Solution.java
+++ Algorithms/#0000 - #0099/#32 - Longest Valid Parentheses/Solution.java
@@ -1,62 +1,76 @@
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 
 /**
- * Implement <b>next permutation</b>, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ * Given a string containing just the characters <tt>'('</tt> and <tt>')'</tt>,
+ * return <i>the length of the longest valid (well-formed) parentheses
+ * substring.</i>
  * <p>
- * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
- * <p>
- * The replacement must be <b>in-place</b> and use only constant extra memory.
- * <p>
- * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
- * <p>
+ * <b>Example 1:</b>
  * <pre>
- * 1,2,3 → 1,3,2
- * 3,2,1 → 1,2,3
- * 1,1,5 → 1,5,1
+ * Input: s = "(()"
+ * Output: 2
+ * Explanation: The longest valid parentheses substring is "()".
  * </pre>
+ * <b>Example 2:</b>
+ * <pre>
+ * Input: s = ")()())"
+ * Output: 4
+ * Explanation: The longest valid parentheses substring is "()()".
+ * </pre>
+ * <b>Example 3:</b>
+ * <pre>
+ * Input: s = ""
+ * Output: 0
+ * </pre>
+ * <ul>
+ * <b>Constraints:</b>
+ * <li><tt>0 <= s.length <= 3 * 10<sup>4</sup></tt></li>
+ * <li><tt>s[i]</tt> is <tt>'('</tt>, or <tt>')'</tt></li>
+ * </ul>
  *
  * @author Oleg Cherednik
- * @since 29.04.2020
+ * @since 16.08.2025
  */
 public class Solution {
 
     public static void main(String... args) {
-//        nextPermutationAndPrint(1, 2, 3);   // [1, 3, 4]
-//        nextPermutationAndPrint(3, 2, 1);   // [1, 2, 3]
-//        nextPermutationAndPrint(1, 1, 5);   // [1, 5, 1]
-
-        nextPermutationAndPrint(1, 3, 2);   // [2, 1, 3]
+        System.out.println(longestValidParentheses("(()")); // 2
+        System.out.println(longestValidParentheses(")()())")); // 4
+        System.out.println(longestValidParentheses("")); // 0
+        System.out.println(longestValidParentheses("()(()"));   // 2
     }
 
-    private static void nextPermutationAndPrint(int... nums) {
-        nextPermutation(nums);
-        System.out.println(Arrays.toString(nums));
-    }
+    public static int longestValidParentheses(String s) {
+        int[] arr = new int[s.length()];
+        Arrays.fill(arr, Integer.MAX_VALUE);
 
-    public static void nextPermutation(int[] nums) {
-        if (nums == null || nums.length < 2)
-            return;
+        Deque<Integer> stack = new LinkedList<>();
 
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (nums[i] >= nums[i + 1])
-                continue;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
 
-            swap(nums, i, i + 1);
-
-            for (int j = i + 2, k = nums.length - 1; j < k; j++, k--)
-                swap(nums, j, k);
-
-            return;
+            if (ch == '(')
+                stack.push(i);
+            else if (ch == ')' && !stack.isEmpty()) {
+                int open = stack.pop();
+                arr[open] = i;
+                arr[i] = -open;
+            }
         }
 
-        for (int j = 0, k = nums.length - 1; j < k; j++, k--)
-            swap(nums, j, k);
-    }
+        int res = 0;
 
-    private static void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            if (arr[i] <= 0) {
+                j += 2;
+                res = Math.max(res, j);
+            } else if (arr[i] == Integer.MAX_VALUE)
+                j = 0;
+        }
+
+        return res;
     }
 
 }
